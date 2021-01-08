152f122860ad2291d0c352c7199e6a1bddf4f6bf
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2021-01-09 02:40:41 +0330

    inital added order and services in domain

---
+++ Code/Source/Diba.Core/Diba.Core.Domain/Order/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diba.Core.Domain
+{
+    public class Order
+    {
+        public long Id { get; private set; }
+        private List<Service> _services { get; set; }
+        public IReadOnlyCollection<Service> Services => _services.AsReadOnly();
+        public OrderState State { get; private set; }
+        public Order()
+        {
+            this._services = new List<Service>();
+            this.State = new DraftState();
+        }
+        public void AddServices(List<Service> service)
+        {
+            if (!State.CanModify())
+                throw new Exception();
+
+            this._services.AddRange(service);
+        }
+        public void AcceptServiceVisitor(IServiceVisitor visitor)
+        {
+            foreach (var service in Services)
+            {
+                service.AcceptVisitor(visitor);
+            }
+        }
+        public void Cancel()
+        {
+            this.State = this.State.Cancel();
+        }
+        public void Confirm()
+        {
+            this.State = this.State.Confirm();
+        }
+
+    }
+}
---
+++ Code/Source/Diba.Core/Diba.Core.Domain/Order/TransportationService.cs
@@ -0,0 +1,15 @@
+﻿namespace Diba.Core.Domain
+{
+    public class TransportationService : Service
+    {
+        public decimal Fee { get; private set; }
+        public TransportationService(int id, string title, decimal fee) : base(id, title)
+        {
+            Fee = fee;
+        }
+        public override void AcceptVisitor(IServiceVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
