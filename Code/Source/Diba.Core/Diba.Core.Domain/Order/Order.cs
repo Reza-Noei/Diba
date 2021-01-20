0a80cac0c4944a3d5a1aa019d16d455a738ae21b
Mohammad Hasan Rashidi <mrrashidi@outlook.com>
2021-01-20 12:33:15 +0330

    added order item

--- Code/Source/Diba.Core/Diba.Core.Domain/Order/Order.cs
+++ Code/Source/Diba.Core/Diba.Core.Domain/Order/Order.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using Diba.Core.Common;
 
 namespace Diba.Core.Domain
 {
     public class Order
     {
         public long Id { get; private set; }
-        private List<Service> _services { get; set; }
-        public IReadOnlyCollection<Service> Services => _services.AsReadOnly();
+        private List<OrderItem> _items;
+        public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+
+        //private List<Service> _services { get; set; }
+        //public IReadOnlyCollection<Service> Services => _services.AsReadOnly();
         public OrderState State { get; private set; }
         public Order()
         {
-            this._services = new List<Service>();
+            this._items = new List<OrderItem>();
             this.State = new CollectedState();
         }
-        public void AddServices(List<Service> service)
-        {
-            if (!State.CanModify())
-                throw new Exception();
 
-            this._services.AddRange(service);
-        }
-        public void AcceptServiceVisitor(IServiceVisitor visitor)
+        public void UpdateItems(List<OrderItem> itmes)
         {
-            foreach (var service in Services)
-            {
-                service.AcceptVisitor(visitor);
-            }
+            this._items.Update(itmes);
         }
+
+        //public void AddServices(List<Service> service)
+        //{
+        //    if (!State.CanModify())
+        //        throw new Exception();
+
+        //    this._services.AddRange(service);
+        //}
+        //public void AcceptServiceVisitor(IServiceVisitor visitor)
+        //{
+        //    foreach (var service in Services)
+        //    {
+        //        service.AcceptVisitor(visitor);
+        //    }
+        //}
     }
+
+
 }
